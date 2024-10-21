0c5f0615c2e0e22b5216f8cf7a2189e31ae66942
mikesant27 <mikesant27@gmail.com>
2024-10-21 14:42:38 -0400

    sep. header into its own .php file

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<div class="menu">
+    <a href="index.html">Home</a>
+    <a href="music.html">Music</a>
+    <a href="tour.html">Tour</a>
+    <a href="merch.html">Merch</a>
+</div>
+
+<div class="header">
+    <h1>Band Name</h1>
+</div>
--- index.php
+++ index.php
@@ -7,15 +7,6 @@
     <title>Band Name Here</title>
 </head>
 <body>
-    <div class="menu">
-        <a href="index.html">Home</a>
-        <a href="music.html">Music</a>
-        <a href="tour.html">Tour</a>
-        <a href="merch.html">Merch</a>
-    </div>
-
-    <div class="header">
-        <h1>Band Name</h1>
-    </div>
+    <?php include './includes/header.php'; ?>
 </body>
 </html>

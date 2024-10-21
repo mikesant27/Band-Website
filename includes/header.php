5f13c0b874be7cd91d10f816024365010724fe57
mikesant27 <mikesant27@gmail.com>
2024-10-21 14:50:13 -0400

    added user to top right corner

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,35 @@
+<?php
+    session_start();
+
+    // Retrieve user data from the session
+    $fullName = $_SESSION['full_name'];
+    $userImage = $_SESSION['user_image'];
+?>
+
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+<style>
+    .user-info {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .user-info img {
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        margin: 5px 0;
+    }
+
+    .logout-link {
+        margin-top: 5px;
+        font-size: 14px;
+    }
+</style>
+
 <div class="menu">
     <a href="index.html">Home</a>
     <a href="music.html">Music</a>
@@ -7,4 +39,13 @@
 
 <div class="header">
     <h1>Band Name</h1>
+    <?php if (isset($_SESSION['user_id'])): ?>
+        <!-- User information displayed at the top right corner -->
+        <div class="user-info">
+            <span><?php echo htmlspecialchars($fullName); ?></span>
+            <?php if (!empty($userImage)): ?>
+                <img src="<?php echo htmlspecialchars($userImage); ?>" alt="User Image">
+            <?php endif; ?>
+        </div>
+    <?php endif; ?>
 </div>

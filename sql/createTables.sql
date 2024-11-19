3adb9250d9a1f5c434e68172ed55c0bc757047eb
mikesant27 <mikesant27@gmail.com>
2024-11-19 09:46:23 -0500

    added products table

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,17 +1,20 @@
-CREATE TABLE `users` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `full_name` varchar(100) DEFAULT NULL,
-  `FileData` longblob DEFAULT NULL,
-  'role' enum('admin','staff','customer'),
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+CREATE TABLE users (
+  id int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  username varchar(50) NOT NULL,
+  email varchar(100) NOT NULL,
+  password varchar(255) NOT NULL,
+  full_name varchar(100) DEFAULT NULL,
+  FileData longblob DEFAULT NULL,
+  role enum('admin','staff','customer'),
+  created_at timestamp NOT NULL DEFAULT current_timestamp(),
+  updated_at timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE 'products'(
-    `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );

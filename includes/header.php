aee295366bc037352d749797cc46b88b7190648c
mikesant27 <mikesant27@gmail.com>
2024-11-21 10:58:34 -0500

    applied boostrap cs to header (couldnt figure it out without)

--- includes/header.php
+++ includes/header.php
@@ -14,6 +14,7 @@
 ?>
 
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <style>
     .user-info {
         position: absolute;

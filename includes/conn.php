86d54f973250046f23916647fd6df95c6c96a6f3
mikesant27 <mikesant27@gmail.com>
2024-11-21 09:59:14 -0500

    added db_name to conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -5,11 +5,11 @@
 $servername = "localhost"; // Or your database server's address
 $username = "root";        // Your MySQL username
 $password = "";            // Your MySQL password
-$dbname = "test";          // Database Name
+$db_name = "test";          // Database Name
 
 try {
     // Create a new PDO instance
-    $conn = new PDO("mysql:host=$servername", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
 
     // Set the PDO error mode to exception to handle errors
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);

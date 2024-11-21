5dbddd28df5db23043cb868062a355e4a20c2363
mikesant27 <mikesant27@gmail.com>
2024-11-21 10:00:25 -0500

    fixed styling and header not opening

--- main/merch/PL/productList.php
+++ main/merch/PL/productList.php
@@ -12,7 +12,7 @@ $products = $controller->listProducts();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product List</title>
-    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../../css/style.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
@@ -25,7 +25,7 @@ $products = $controller->listProducts();
 </head>
 
 <body>
-    <?php include '../../includes/header.php'; ?>
+    <?php include '../../../includes/header.php'; ?>
     <div class="container my-5">
         <div class="d-flex justify-content-between align-items-center">
             <h1>Product List</h1>

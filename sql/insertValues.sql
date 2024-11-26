312642fdfcb42b3c9b187f8343fba80259073f13
Michael <mikesant27@gmail.com>
2024-11-26 17:32:27 -0500

    changed file to insert values

---
+++ sql/insertValues.sql
@@ -0,0 +1,11 @@
+INSERT INTO products (name, price, description) VALUES
+('Band Logo T-Shirt', 25.99, 'This is a test item'),
+('Vinyl Album', 45.99, 'This is another test item');
+
+
+INSERT INTO shows (location, show_time) VALUES
+('Hartford','2024-12-01 08:00:00'),
+('Philadelphia','2024-12-02 08:00:00'),
+('Miami','2024-12-03 08:00:00'),
+('New Orleans','2024-12-04 08:00:00'),
+('Dallas','2024-12-05 08:00:00');

f9c8c0c0c030c7dee9d7d02cba2d1695e061f2fe
Michael <mikesant27@gmail.com>
2024-11-26 19:24:28 -0500

    added timestamps to all tables

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -16,7 +16,8 @@ CREATE TABLE products (
   name VARCHAR(255) NOT NULL,
   price DECIMAL(10, 2) NOT NULL,
   description TEXT,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 );
 
 CREATE TABLE ALBUM (
@@ -25,7 +26,9 @@ CREATE TABLE ALBUM (
   artist_name VARCHAR(255) NOT NULL,
   release_date DATE,
   genre VARCHAR(100),
-  album_cover longblob DEFAULT NULL
+  album_cover longblob DEFAULT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 );
 
 CREATE TABLE SONG (
@@ -34,13 +37,17 @@ CREATE TABLE SONG (
   duration TIME NOT NULL,
   album_id INT,
   track_num INT,
-  FOREIGN KEY (album_id) REFERENCES ALBUM(id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (album_id) REFERENCES ALBUM(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 );
 
 CREATE TABLE SHOWS(
   id INT AUTO_INCREMENT PRIMARY KEY,
   location VARCHAR(255) NOT NULL,
-  show_time DATETIME NOT NULL
+  show_time DATETIME NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 );
 
 CREATE TABLE transactions (
@@ -49,7 +56,9 @@ CREATE TABLE transactions (
   service_type VARCHAR(100),
   date_time DATETIME,
   status ENUM('scheduled','completed','canceled') NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 );
 
 /*

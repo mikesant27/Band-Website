ecc03d8115bc8231405911cc2ade8c0108449354
ArbinAsipi <arbinasipi03@gmail.com>
2024-11-27 10:14:28 -0500

    add blog table

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -61,6 +61,16 @@ CREATE TABLE transactions (
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+CREATE TABLE blog (
+    id INT AUTO_INCREMENT PRIMARY KEY,        
+    title VARCHAR(255) NOT NULL,              
+    content TEXT NOT NULL,                    
+    author_id INT UNSIGNED NOT NULL,          
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+    FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
 /*
 Maybe change service_type to products bought?
 Transactions table not working

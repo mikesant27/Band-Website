1d591a2dc8e0c08e3e2765f1a32ca866feb39adc
ArbinAsipi <arbinasipi03@gmail.com>
2024-11-27 12:34:02 -0500

    Added favicon

--- main/home/PL/home.php
+++ main/home/PL/home.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../css/style.css">
     <title>Band Name Here</title>
+    <link rel="icon" type="image/x-icon" href="../../../includes/favicon.png">
 </head>
 <body>
     <?php include '../../../includes/header.php'; ?>
--- main/merch/PL/productList.php
+++ main/merch/PL/productList.php
@@ -12,6 +12,7 @@ $products = $controller->listProducts();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product List</title>
+    <link rel="icon" type="image/x-icon" href="../../../includes/favicon.png">
     <link rel="stylesheet" href="../../../css/style.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">

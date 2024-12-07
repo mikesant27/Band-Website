5f91d3d0b2cf45f14f900d2dd8b9e0c03db119a9
ArbinAsipi <arbinasipi03@gmail.com>
2024-12-07 14:34:17 -0500

    Started working on home page

--- main/home/PL/main_home.php
+++ main/home/PL/main_home.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="../../../css/style.css">
 
 <center><h2 id="Welcome">Welcome</h2></center>
+<p id="Home">Greeting, this is a test to see what it would look like for people viewing the page.</p>

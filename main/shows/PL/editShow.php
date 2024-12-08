68ed2c2e62e316c0ddff64ac68cf2067b4bb1d73
Michael <mikesant27@gmail.com>
2024-12-08 16:49:04 -0500

    all shows styling is fixed

--- main/shows/PL/editShow.php
+++ main/shows/PL/editShow.php
@@ -41,6 +41,7 @@ if (isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Show</title>
+    <link rel="stylesheet" href="../../../css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 

f009d1a08dea15424f7556b086d953b236cfcc81
ArbinAsipi <arbinasipi03@gmail.com>
2024-12-08 12:35:44 -0500

    updated line break

--- main/home/PL/main_home.php
+++ main/home/PL/main_home.php
@@ -2,7 +2,7 @@
 
 <center><h2 id="Welcome">Welcome</h2></center>
 <p id="Home">Say hello to Band Name. <br><br>They've been a band since highschool where they started small in their garage, but
-    now they're one of the hottest bands in all of music. They're mainly a rock band but they've dabbled in many genres. <br>
+    now they're one of the hottest bands in all of music. They're mainly a rock band but they've dabbled in many genres. <br> <br>
     So take a look around, buy some merch, check out the members blogs, see where they're be playing next, and 
     get to know them now because soon, the whole word will know their name and be rocking out to their songs.
 </p>

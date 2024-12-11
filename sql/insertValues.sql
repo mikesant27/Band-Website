42e022118eeedb055791a62f3d455d73783071dd
Michael <mikesant27@gmail.com>
2024-12-11 12:16:22 -0500

    added products

--- sql/insertValues.sql
+++ sql/insertValues.sql
@@ -1,6 +1,11 @@
 INSERT INTO products (name, price, description) VALUES
-('Band Logo T-Shirt', 25.99, 'This is a test item'),
-('Vinyl Album', 45.99, 'This is another test item');
+('Band Logo T-Shirt', 25.99, 'Soft and comfortable band-themed t-shirts featuring eye-catching designs and the band logo—perfect for any fan’s wardrobe'),
+('Band Logo Hoodie', 49.99, 'Cozy and stylish hoodies to keep you warm while repping your favorite band. Available in a variety of colors and sizes.'),
+('Poster', 9.99, '18"x24" High-quality posters showcasing unique artwork, album covers, or tour dates to decorate your space in style.'),
+('Stickers (Set of 5)', 4.99, 'Fun and vibrant sticker sets featuring iconic designs, perfect for laptops, water bottles, or any surface needing a personal touch.'),
+('Signed Setlist', 14.99, 'A unique collectible—a printed setlist from a recent concert, signed by the band members themselves.'),
+('CD Album', 14.99, 'Compact and portable, these CD albums include all the hits and come with a booklet of lyrics and photos'),
+('Vinyl Album', 45.99, 'A collector’s must-have! Premium vinyl records of the band’s latest or classic albums with exclusive cover art.');
 
 
 INSERT INTO shows (location, show_time) VALUES
